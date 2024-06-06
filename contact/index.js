b094c7c16cd6d5fd9932bcd5d931f1f95c817e61
anandamarwan <anandapuspita2@gmail.com>
2024-06-06 13:09:42 +0700

    feat: show contact details

--- contact/index.js
+++ contact/index.js
@@ -21,14 +21,20 @@ function renderContactById() {
   }
 
   contactContainerElement.innerHTML = `
-<h2>${contact.fullName}</h2>
-<p>${contact.email}</p>
-<p>${contact.phone}</p>
-<p>${contact.birthday}</p>
-<div>
-  <button onclick="renderEditContactFormById(${contact.id})">Edit</button>
-  <button onclick="deleteContactById(${contact.id})">Delete</button>
-</div>
+  <main class="bg-white">
+  <div>
+  <button onclick="renderEditContactFormById(${contact.id})" class="p-2.5 bg-blue-700 hover:bg-blue-500 text-white rounded-full">Edit</button>
+  <button onclick="deleteContactById(${contact.id})">🗑️</button>
+  </div>
+  <h2 class="text-3xl">${contact.fullName}</h2>
+  <div class="p-4 bg-gray-400 rounded-lg">
+  <h3>Contacts details</h3>
+  <p>✉︎ ${contact.email}</p>
+  <p>☎️ ${contact.phone}</p>
+  <p>🎂 ${contact.birthday}</p>
+  </div>
+  </main>
+
   `;
 }
 
@@ -38,35 +44,40 @@ function renderEditContactFormById(id) {
   contactContainerElement.innerHTML = `
 <form id="edit-contact-form" method="post">
   <div>
-    <label for="full-name">Full name:</label>
+    <label for="full-name">Full Name</label>
     <input
       id="full-name"
       name="fullName"
       type="text"
+      class="bg-gray-100 w-96 p-4 rounded-lg"
       value="${contact.fullName}"
     />
   </div>
   <div>
-    <label for="email">Email address:</label>
+    <label for="email">Email</label>
     <input
       id="email"
       name="email"
       type="email"
+      class="bg-gray-100 w-96 p-4 rounded-lg"
       value="${contact.email}"
     />
   </div>
   <div>
-    <label for="phone">Phone number:</label>
+    <label for="phone">Phone</label>
     <input id="phone" name="phone" type="phone" 
+    class="bg-gray-100 w-96 p-4 rounded-lg"
     value="${contact.phone}"
     />
   </div>
   <div>
-    <label for="birthday">birthday</label>
+    <label for="birthday">Birthday</label>
     <input id="birthday" name="birthday" 
+    type="date"
+    class="bg-gray-100 w-96 p-4 rounded-lg"
     value="${contact.birthday}" />
   </div>
-  <button type="submit">Save</button>
+  <button type="submit" class="p-2.5 bg-blue-700 hover:bg-blue-500 text-white rounded-full">Save</button>
 </form>`;
 
   const editContactFormElement = document.getElementById("edit-contact-form");
@@ -85,7 +96,7 @@ function editContact(event) {
     fullName: contactFormData.get("fullName"),
     email: contactFormData.get("email"),
     phone: contactFormData.get("phone"),
-    birthday: Number(contactFormData.get("birthday")),
+    birthday: contactFormData.get("birthday"),
   };
 
   // Update by using map, to find by id, not adding a new one
